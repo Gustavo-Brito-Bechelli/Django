be2610144d3c88bc21b129fe1050e33f8dcf136b
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-02-24 10:11:56 -0300

    feat(views, urls): criando segmentos dinamicos

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Funcionou!")
-
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minuts avery day!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'Walk for at least 20 minuts avery day!'
+    elif month == 'february':
+        challenge_text = 'Study Django for 20 min every day'
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
