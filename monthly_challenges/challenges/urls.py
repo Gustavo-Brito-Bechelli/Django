9ce0a16ef647bdf13b260185fe5053c2235a2504
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-02-24 11:28:31 -0300

    feat(challenges): utilizando funcao reverse

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name='month-challenge')
 ]
