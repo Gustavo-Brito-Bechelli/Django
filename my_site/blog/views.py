0304793d50e6414b053ca95a4311b11ac2419466
Gustavo Brito <gustavobrito.bc@gmail.com>
2025-03-05 11:50:28 -0300

    feat(my_site): carregando dados mais dinamicamente

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,101 @@
+from datetime import date
+
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "Hike-in-the-mountains",
+        "image": "mountain.pjg",
+        "author": "Gustavo",
+        "date": date(2025, 3, 5),
+        "title": "Mountain Hiking",
+        "excerpt": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.""",
+        "content": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+        """
+    }, 
+    {
+        "slug": "Coding",
+        "image": "coding.pjg",
+        "author": "Gustavo",
+        "date": date(2021, 7, 25),
+        "title": "Coding can be fun!",
+        "excerpt": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.""",
+        "content": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+        """
+    }, 
+    {
+        "slug": "Nature",
+        "image": "woods.pjg",
+        "author": "Gustavo",
+        "date": date(2024, 4, 10),
+        "title": "Nature is great!",
+        "excerpt": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.""",
+        "content": """
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+
+        lorenLorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore 
+            magna aliqua.
+        """
+    }
+]
+
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html", {
+        "post": identified_post
+    })
